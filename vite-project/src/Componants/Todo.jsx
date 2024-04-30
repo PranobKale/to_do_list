cb2b724e9f5dd59d8398fdef3fc222138afad1bc
pranobkale <pranob.kale@decimalpointanalytics.com>
2024-04-30 20:50:07 +0530

    state related changes

--- vite-project/src/Componants/Todo.jsx
+++ vite-project/src/Componants/Todo.jsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
 import  "./CSS/Todo.css";
+import { useRef } from "react";
+import { useEffect } from "react";
+import TodoItems from "./TodoItems";
 
+let count = 0;
 const Todo = () => {
-  return (
+    
+    const [todo, setTodo] = useState([]);
+    
+    const inputRef = useRef(null);
+    
+    const add = () => {
+        setTodo([...todo,{no:count++, text:inputRef.current.value,display:""}]);
+        inputRef.current.value = "";
+        localStorage.setItem("todo-count",count)
+    }
+
+    useEffect(() =>{
+        setTodo(JSON.parse(localStorage.getItem("todo")))
+        count = localStorage.getItem("todo-count")
+        // setTodo(JSON.parse(localStorage.getItem("todo-count")))
+    },[])
+
+    useEffect(() => {
+        setTimeout(() => {
+            localStorage.setItem("todo", JSON.stringify(todo));
+        },100)
+    },[todo])
+    
+    return (
+
     <div className="todo">
         <div className="todo-header">To-Do List</div>
         <div className="todo-add">
-            <input type="text" placeholder="Add Your Task" className="todo-input"/>
-            <div className="todo-add-btn">ADD</div>
+            <input ref={inputRef} type="text" placeholder="Add Your Task" className="todo-input"/>
+            <div onClick={add} className="todo-add-btn">ADD</div>
         </div>
         <div className="todo-list">
-            
+        {todo.map((item, index) => {
+            return <TodoItems key={index} setTodo={setTodo} no={item.no}  display={item.display} text={item.text} />
+}
+        )}
         </div>
 
 
--- vite-project/src/Componants/TodoItems.jsx
+++ vite-project/src/Componants/TodoItems.jsx
@@ -1,9 +1,34 @@
 import  "./CSS/TodoItems.css"
+import tick from "./Assets/tick.png";
+import cross from  "./Assets/cross.png";
+import circle from  "./Assets/circle.png";
 
-const TodoItems = () => {
-  return (
-    <div>TodoItems</div>
-  )
+const TodoItems = ({no,display,text,setTodo}) => {
+
+    const toggel = (no) => {
+        const data = JSON.parse(localStorage.getItem('todo'));
+        for (let i = 0; i < data.length; i++) {
+        if (data[i].no === no) {
+            display = "line-through";
+        }
+        else{
+            display = "";
+        }
+        break;
+       }
+       setTodo(data);
+}
+
+
+    return (
+        <div className="todoitems">
+            <div className="toditems-container" onClick={() => {toggel(no)}}>
+                {display===""?<img src={circle} alt=""/>:<img src={tick} alt=""/>} 
+                <div className="todoitems-text">{text}</div>
+            </div>
+            <img className="todoitems-cross-item" src={cross} alt="" />
+        </div>
+    );
 }
 
 export default TodoItems
